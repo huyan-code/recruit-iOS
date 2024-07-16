4fbb20d9011c9bdff229feb95981db0e101a5925
huyan <34530704+Tig01er@users.noreply.github.com>
2024-07-17 11:27:53 +1200

    [add] the Transaction model

---
+++ ASBInterviewExercise/Models/Transaction.swift
@@ -0,0 +1,37 @@
+//
+//  Transaction.swift
+//  ASBInterviewExercise
+//
+//  Created by yanhu on 17/07/24.
+//
+
+import Foundation
+
+// Transaction Model
+struct Transaction: Codable {
+    let id: Int
+    let transactionDate: String
+    let summary: String
+    let debit: Double
+    let credit: Double
+    
+    // computed properties to determine the type of transaction
+    var isDebit: Bool {
+        return debit > 0
+    }
+    
+    var amount: Double {
+        return isDebit ? debit : credit
+    }
+    
+    var gst: Double {
+        return amount * 0.15 / 1.15
+    }
+    
+    var date: Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        return dateFormatter.date(from: transactionDate) ?? Date()
+    }
+}
+
