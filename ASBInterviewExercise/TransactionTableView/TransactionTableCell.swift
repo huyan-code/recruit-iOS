0ed04da8b01da32cab1b139ac74d2f4b90476ef3
huyan <34530704+Tig01er@users.noreply.github.com>
2024-07-17 12:17:39 +1200

    [add] TransactionTableView and TransactionTableCell

---
+++ ASBInterviewExercise/TransactionTableView/TransactionTableCell.swift
@@ -0,0 +1,107 @@
+//
+//  TransactionTableCell.swift
+//  ASBInterviewExercise
+//
+//  Created by yanhu on 17/07/24.
+//
+
+import UIKit
+import SnapKit
+
+class TransactionTableCell: UITableViewCell {
+    
+    // MARK: - Properties
+    let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let summaryLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        label.textColor = .black
+        label.numberOfLines = 3
+        label.lineBreakMode = .byTruncatingTail
+        return label
+    }()
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .gray
+        return label
+    }()
+    
+    let amountLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        label.textColor = .black
+        label.textAlignment = .right
+        return label
+    }()
+    
+    // MARK: - Initialization
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupSubviews()
+        setupConstraints()
+        accessoryType = .disclosureIndicator // Add disclosure indicator
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    private func setupSubviews() {
+        contentView.addSubview(iconImageView)
+        contentView.addSubview(summaryLabel)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(amountLabel)
+    }
+    
+    private func setupConstraints() {
+        let padding: CGFloat = 16.0
+        
+        iconImageView.snp.makeConstraints { make in
+            make.leading.equalTo(contentView.safeAreaLayoutGuide).offset(padding)
+            make.centerY.equalTo(contentView)
+            make.width.height.equalTo(24)
+        }
+        
+        amountLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(contentView.safeAreaLayoutGuide).offset(-padding)
+            make.centerY.equalTo(contentView)
+        }
+        
+        summaryLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentView.safeAreaLayoutGuide).offset(padding)
+            make.leading.equalTo(iconImageView.snp.trailing).offset(padding)
+            make.trailing.equalTo(amountLabel.snp.leading).offset(-padding)
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(summaryLabel.snp.bottom).offset(4)
+            make.leading.equalTo(summaryLabel)
+            make.trailing.equalTo(summaryLabel)
+            make.bottom.equalTo(contentView.safeAreaLayoutGuide).offset(-padding)
+        }
+    }
+    
+    // MARK: - Configure Cell
+    func configure(summary: String, date: String, amount: String, isCredit: Bool) {
+        summaryLabel.text = summary
+        dateLabel.text = date
+        amountLabel.text = amount
+        
+        if isCredit {
+            iconImageView.image = UIImage(named: "icon-credit")
+            amountLabel.textColor = .green
+        } else {
+            iconImageView.image = UIImage(named: "icon-debit")
+            amountLabel.textColor = .red
+        }
+    }
+}
---
+++ ASBInterviewExercise/TransactionTableView/TransactionTableView.swift
@@ -0,0 +1,82 @@
+//
+//  TransactionTableView.swift
+//  ASBInterviewExercise
+//
+//  Created by yanhu on 17/07/24.
+//
+
+import UIKit
+
+class TransactionTableView: UIView {
+    private let tableView = UITableView()
+    private let tableCellId = "TransactionTableCell"
+    var isInteractionEnabled = true
+
+    var viewModel: TransactionTableViewModel? {
+        didSet {
+            reloadData()
+        }
+    }
+    
+    var didSelectTransaction: ((Transaction) -> Void)?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupTableView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        addSubview(tableView)
+        tableView.register(TransactionTableCell.self, forCellReuseIdentifier: tableCellId)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func reloadData() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension TransactionTableView: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.transactions.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableCellId, for: indexPath)
+        if let transaction = viewModel?.transactions[indexPath.row], let transactionCell = cell as? TransactionTableCell {
+            
+            let color = transaction.isDebit ? UIColor.red : UIColor.green
+            let iconStr = transaction.isDebit ? "icon-debit" : "icon-credit"
+            
+            transactionCell.configure(summary: transaction.summary, date: transaction.date.description, amount: "\(transaction.amount)", isCredit: !transaction.isDebit)
+
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        guard isInteractionEnabled else { return }
+        isInteractionEnabled = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.isInteractionEnabled = true
+        }
+        
+        guard let transaction = viewModel?.transactions[indexPath.row] else { return }
+        didSelectTransaction?(transaction)
+    }
+    
+}
